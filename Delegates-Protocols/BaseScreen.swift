0601977f327a532050643d71db759c8234244c26
mohamed <mohamed@faress-mac-3.local>
2019-01-06 17:00:07 +0200

    1

---
+++ Delegates-Protocols/BaseScreen.swift
@@ -0,0 +1,38 @@
+
+
+import UIKit
+
+class BaseScreen: UIViewController {
+
+    @IBOutlet weak var mainImageView: UIImageView!
+    @IBOutlet weak var chooseButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        chooseButton.layer.cornerRadius = chooseButton.frame.size.height/2
+    }
+
+    @IBAction func chooseButtonTapped(_ sender: UIButton) {
+        let selectionVC = storyboard?.instantiateViewController(withIdentifier: "SelectionScreen") as! SelectionScreen
+        //set selection deleget whic base vc say to selection i will be your deleget
+        selectionVC.selectionDelegate=self
+       
+        present(selectionVC, animated: true, completion: nil)
+    }
+}
+extension BaseScreen:SideSelectionDelegate{
+    func didTapChoice(image: UIImage, name: String, color: UIColor) {
+        mainImageView.image=image
+        nameLabel.text=name
+        view.backgroundColor=color
+        
+    }
+    
+        
+    }
+
+
+
+    
+
